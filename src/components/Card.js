bfd25f0467f278ed73177ec22bcc06eef4bf3b51
Akanksha Singh <akanksha.sngh660@gmail.com>
2022-02-17 03:01:30 +0530

    made some changes in weather app

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Card, Feed } from 'semantic-ui-react'
 import moment from 'moment'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCloud} from '@fortawesome/free-solid-svg-icons' 
 
-const WeatherCard = ({temperature, humidity, sunrise, sunset, city, icon}) => (
+const WeatherCard = ({temperature, humidity, sunrise, sunset, city}) => (
+  
   <Card>
-    <Card.Content>
-      <Card.Header>{city}</Card.Header>
+    <Card.Content className="weather-card">
+      <Card.Header className="city-header">{city}</Card.Header>
+      <div className="icon-container">
+        <FontAwesomeIcon icon={faCloud} size="xl"/>
+
+      </div>
     </Card.Content>
     <Card.Content>
       <Feed>
@@ -13,17 +20,15 @@ const WeatherCard = ({temperature, humidity, sunrise, sunset, city, icon}) => (
           <Feed.Content>
             <h4 className="weather-card-child">{moment().format('lll')}</h4>
             <div className="weather-card">
-                <div className="weather-card-child">{Math.floor(temperature)}°C</div>
-                <div className="weather-card-child">{humidity}%</div>
+                <div className="weather-card-child">Temperature: {Math.floor(temperature)}°C</div>
+                <div className="weather-card-child">Humidity: {humidity}%</div>
             </div>
             <div className="weather-card">
-                <div className="weather-card-child">{new Date(sunrise * 1000).toLocaleTimeString('en-IN')}</div>
-                <div className="weather-card-child">{new Date(sunset * 1000).toLocaleTimeString('en-IN')}</div>
+                <div className="weather-card-child">Sunrise: {new Date(sunrise * 1000).toLocaleTimeString('en-IN')}</div>
+                <div className="weather-card-child">Sunset: {new Date(sunset * 1000).toLocaleTimeString('en-IN')}</div>
             </div>             
           </Feed.Content>
-        </Feed.Event>
-
-        
+        </Feed.Event>        
       </Feed>
     </Card.Content>
   </Card>
